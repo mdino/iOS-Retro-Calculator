f59d8379466b51e34bb4451a1e43743c2d121575
Dino <dinomusliu@Dinos-MacBook-Pro-2.local>
2016-06-28 01:24:25 +0200

    Calculator Finished
    Dodana je funkcionalnost

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,19 +7,132 @@
 //
 
 import UIKit
+import AVFoundation // audio video player
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputlbl: UILabel!
+    var btnSound : AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftVarStr = ""
+    var rightVarStr = ""
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Add = "+"
+        case Subtruct = "-"
+        case Empty = ""
+    
+    }
+    
+    var currentOperation : Operation = Operation.Empty
+    var result = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        
+        do{
+            try btnSound = AVAudioPlayer (contentsOfURL: soundURL)
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPressed(btn : UIButton!){
+        playSound()
+        
+        runningNumber += "\(btn.tag)"
+        outputlbl.text = runningNumber
     }
-
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        proccessOperation(Operation.Divide)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        proccessOperation(Operation.Subtruct)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        proccessOperation(Operation.Multiply)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        proccessOperation(Operation.Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        proccessOperation(currentOperation)
+        
+    }
+    
+    @IBAction func btnClear(sender: AnyObject) {
+        playSound()
+        leftVarStr = "0"
+        outputlbl.text = "0"
+        proccessOperation(currentOperation)
+    }
+    
+    func proccessOperation (op: Operation){
+        playSound()
+        
+        if currentOperation != Operation.Empty{
+        // racun
+            
+            if( runningNumber != ""){
+                
+                rightVarStr = runningNumber
+                runningNumber = ""
+      
+                if(currentOperation == Operation.Multiply)
+                {
+                    result = "\(Double(leftVarStr)! * Double(rightVarStr)!)"
+                }
+                else if(currentOperation == Operation.Add)
+                {
+                    result = "\(Double(leftVarStr)! + Double(rightVarStr)!)"
+                }
+                else if(currentOperation == Operation.Divide){
+                    result = "\(Double(leftVarStr)! / Double(rightVarStr)!)"
+                }
+                else if(currentOperation == Operation.Subtruct){
+                    result = "\(Double(leftVarStr)! - Double(rightVarStr)!)"
+                }
+            
+                leftVarStr = result
+                outputlbl.text = result
+                
+                } // if runningnumber
+            
+            currentOperation = op
+        
+        }//if currentOperation !=
+            
+        else {
+        // prvi puta je operator pretisnut
+           leftVarStr = runningNumber
+           runningNumber = ""
+           currentOperation = op
+        }
+        
+    
+    }
+    
+    func playSound()
+    {
+        if btnSound.playing
+        {
+            btnSound.stop()
+        }
+        btnSound.play()
+    
+    }
 
 }
 
